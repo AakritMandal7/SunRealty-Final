1e760fae4b2ab5e91cd15909c41dbd2681b4241e
AakritMandal7 <aakritmandal2340@gmail.com>
2025-08-02 15:46:44 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,14 +1,13 @@
 const toggle = document.getElementById('menu-toggle');
 const navbar = document.getElementById('navbar');
 
-// Toggle menu on hamburger click
+// Toggle the mobile menu
 toggle.addEventListener('click', () => {
   navbar.classList.toggle('active');
 });
 
-// Close menu when any navbar link is clicked
+// Close the menu when a link is clicked (only for mobile view)
 const navLinks = navbar.querySelectorAll('a');
-
 navLinks.forEach(link => {
   link.addEventListener('click', () => {
     navbar.classList.remove('active');
